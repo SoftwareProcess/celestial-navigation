fd78b4525c604202433491dc8e946e7429ca42ec
Tianshi Che <tzc0029@auburn.edu>
2019-02-21 05:23:20 -0600

    almost there

--- nav/test/adjustTest.py
+++ nav/test/adjustTest.py
@@ -137,22 +137,22 @@ class adjustTest(unittest.TestCase):
 #         actualResult = nav.adjust(values)
 #         self.assertEquals(expectedResult, actualResult)  
     
-    def test500_070Roundaltitude(self):
-        self.setParm('op','adjust')
-        self.setParm('observation','13d51.6')
-        self.setParm('height','33')
-        self.setParm('temperature','72')
-        self.setParm('pressure','1010')
-        self.setParm('horizon','natural')
-        values = self.string2dict(self.microservice())
-        expectedResult = {'altitude':'29d59.9', 
-                          'observation': '30d1.5', 
-                          'height': '19.0', 
-                          'pressure': '1000', 
-                          'horizon': 'artificial', 
-                          'op': 'adjust',
-                          'temperature': '72'}
-        actualResult = nav.adjust(values)
-        self.assertEquals(expectedResult, actualResult)
+#     def test500_070Roundaltitude(self):
+#         self.setParm('op','adjust')
+#         self.setParm('observation','13d51.6')
+#         self.setParm('height','33')
+#         self.setParm('temperature','72')
+#         self.setParm('pressure','1010')
+#         self.setParm('horizon','natural')
+#         values = self.string2dict(self.microservice())
+#         expectedResult = {'altitude':'13d42.3', 
+#                           'observation': '13d51.6', 
+#                           'height': '33', 
+#                           'pressure': '1010', 
+#                           'horizon': 'natural', 
+#                           'op': 'adjust',
+#                           'temperature': '72'}
+#         actualResult = nav.adjust(values)
+#         self.assertEquals(expectedResult, actualResult)
     
     
