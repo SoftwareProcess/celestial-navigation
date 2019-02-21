5462f1602c84051e956443f567be7ce35b15e72a
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 17:43:13 -0600

    Green Light |

--- nav/dispatch.py
+++ nav/dispatch.py
@@ -38,9 +38,9 @@ def dispatch(values=None):
         values['error'] = 'height is invalid'
         return values
     
-#     if (not(values['temperature'].isdigit())):
-#         values['error'] = 'temperature is not a digit'    
-#         return values
+    if (not(values['temperature'].isdigit())):
+        values['error'] = 'temperature is not a digit'    
+        return values
     if (int(values['temperature']) > 120):
         values['error'] = 'temperature is out of bound'
         return values
