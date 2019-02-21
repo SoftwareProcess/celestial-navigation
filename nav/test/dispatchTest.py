5e83b73e7757795e14db3872dee1e38023e566a7
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 16:13:26 -0600

    Red Light |

--- nav/test/dispatchTest.py
+++ nav/test/dispatchTest.py
@@ -160,7 +160,7 @@ class DispatchTest(unittest.TestCase):
         resultDictionary = self.string2dict(result)
         self.assertTrue(resultDictionary.has_key("error"), True)
         
-    def test100_953ShouldReturnValuesWithErrorWhenObservationYOutOfUpperBound(self):
+    def test100_943ShouldReturnValuesWithErrorWhenObservationYOutOfUpperBound(self):
         self.setParm('op','adjust')
         self.setParm('observation','45d60')        
         result = self.microservice()
