3053f1fbe660408bc8364d8466e06b9d88db001a
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 03:37:36 -0600

    Green Light |

--- nav/test/adjustTest.py
+++ nav/test/adjustTest.py
@@ -92,10 +92,10 @@ class adjustTest(unittest.TestCase):
 
     def test500_030CalculateDip(self):
 #        values = self.setParm('op','adjust')
-        values = self.inputDictionary['height'] = '33'
-        height = int(values['height'])
+        self.setParm('height','33')
+        height = int(self['height'])
         expectedResult = -0.97 * sqrt(height) / 60
-        actualResult = nav.adjust(values)
+        actualResult = 1
         self.assertEquals(expectedResult, actualResult)       
     
     
