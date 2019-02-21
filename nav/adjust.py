dd5a256582303807ba8951382e11b5ca8c93ed7c
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 04:18:19 -0600

    Green Light |

--- nav/adjust.py
+++ nav/adjust.py
@@ -1,9 +1,19 @@
-from cmath import sqrt
+from cmath import sqrt, atan, pi
 
 def adjust(values = None):
     
-    dip = -0.97 * sqrt(int(values['height'])) / 60
+    degree, minut =  values['observation'].split('d')
+    minutes = minut + degree * 60
+    radians = minutes * pi / (60 * 180)
+    height = float(values['height'])
+    temperature = int(values['temperature'])
+    pressure = int(values['pressure'])
     
     
-    result = dip
+    
+    dip = -0.97 * sqrt(height) / 60   
+    refraction = (-0.00452 * pressure) / (273 + (temperature-32) * 5 / 9) / atan(radians)
+    
+    
+    result = refraction
     return result
