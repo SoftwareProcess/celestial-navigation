5d2ec8b17b23bfd8727291c10cc685e016bac91a
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 16:10:43 -0600

    Green Light |

--- nav/test/dispatchTest.py
+++ nav/test/dispatchTest.py
@@ -139,6 +139,11 @@ class DispatchTest(unittest.TestCase):
         result = self.microservice()
         resultDictionary = self.string2dict(result)
         self.assertTrue(resultDictionary.has_key("error"), True)
- 
+    
+    def test100_950ShouldReturnValuesWithErrorWhenObservationMissing(self):
+        self.setParm('op','adjust')        
+        result = self.microservice()
+        resultDictionary = self.string2dict(result)
+        self.assertTrue(resultDictionary.has_key("error"), True)
     
 
