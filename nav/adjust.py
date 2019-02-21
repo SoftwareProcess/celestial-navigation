9e0a34f4d42bc144ab84a9099d6e37f271b578db
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 04:27:02 -0600

    Green Light |

--- nav/adjust.py
+++ nav/adjust.py
@@ -3,7 +3,7 @@ from cmath import sqrt, atan, pi
 def adjust(values = None):
     
     degree, minut =  values['observation'].split('d')
-    minutes = int(minut) + int(float(degree * 60))
+    minutes = int(float(minut)) + int(degree * 60)
     radians = minutes * pi / (60 * 180)
     height = int(values['height'])
     temperature = int(values['temperature'])
