5c77d9b6d89713de3fb10cace8d40ec8d650c29d
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 16:23:34 -0600

    Green Light |

--- nav/dispatch.py
+++ nav/dispatch.py
@@ -21,7 +21,7 @@ def dispatch(values=None):
     if (not(values.has_key('observation'))):
         values['error'] = 'observation is missing'
         return values
-    if (not(values['observation'].contains('d'))):
+    if ( 'd' not in values['observation']):
         values['error'] = 'observation does not contain d'
         return values
     x, y = values['observation'].split('d')
