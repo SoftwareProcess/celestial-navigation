76ccce9a9b34fd7d2b3043004cff55f157430ab9
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 18:50:47 -0600

    Green Light |

--- nav/dispatch.py
+++ nav/dispatch.py
@@ -32,7 +32,7 @@ def dispatch(values=None):
     
     if (not(values.has_key('height'))):
         values['height'] = '0' 
-    if (not(isint(values['height'])) and not(isfloat(values['height']))):
+    if (values['height'].isdigit()):
         values['error'] = 'height is not numeric'    
         return values 
     if (float(values['height']) < 0):
