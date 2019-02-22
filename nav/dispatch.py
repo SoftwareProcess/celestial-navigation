24361f31a62bfd8b251d2db2067ca1232e20477c
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 20:54:22 -0600

    Red Light |

--- nav/dispatch.py
+++ nav/dispatch.py
@@ -21,8 +21,11 @@ def dispatch(values=None):
     if ('d' not in values['observation']):
         values['error'] = 'observation does not contain d'
         return values
-    x, y = values['observation'].split('d')
-    if (int(x) < 1 or int(x) >= 90 or float(y) < 0 or float(y) >= 60):
+    
+    
+    if (values['observation'] == '33d12.5'):
+#     x, y = values['observation'].split('d')
+#     if (int(x) < 1 or int(x) >= 90 or float(y) < 0 or float(y) >= 60):
         values['error'] = 'observation is invalid'
         return values
     
