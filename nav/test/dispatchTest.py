c3a9b9eed6e9d2c0456ac9b9eecea040b92010b0
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 22:09:19 -0600

    Green Light |

--- nav/test/dispatchTest.py
+++ nav/test/dispatchTest.py
@@ -164,11 +164,18 @@ class DispatchTest(unittest.TestCase):
     def test100_943ShouldReturnValuesWithErrorWhenObservationYOutOfUpperBound(self):
         self.setParm('op','adjust')
         self.setParm('observation','45d12')
-        self.setParm('height','10')      
+        self.setParm('height','33')
+        self.setParm('temperature','72')
+        self.setParm('pressure','1010')
+        self.setParm('horizon','natural')      
           
         actualResult = adjust.adjust(self.inputDictionary)
         expectedResult = { 'observation': '45d12',                  
                            'op': 'adjust',
+                           'height': '33', 
+                           'pressure': '1010', 
+                           'horizon': 'natural',
+                           'temperature': '72',
                            'error': 'observation is in valid'}       
         self.assertEquals(expectedResult, actualResult)
                
