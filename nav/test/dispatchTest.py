a7253c9ac9dcab72b98294924d47ce7e5c668c8d
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 22:43:24 -0600

    Green Light |

--- nav/test/dispatchTest.py
+++ nav/test/dispatchTest.py
@@ -170,14 +170,13 @@ class DispatchTest(unittest.TestCase):
         self.setParm('observation','90d15.2')        
         result = nav.dispatch(self.inputDictionary)
         self.assertTrue(result.has_key("error"), True)
-#         
-#     def test100_942ShouldReturnValuesWithErrorWhenObservationXOutOfLowerBound(self):
-#         self.setParm('op','adjust')
-#         self.setParm('observation','0d15.2')        
-#         result = self.microservice()
-#         resultDictionary = self.string2dict(result)
-#         self.assertTrue(resultDictionary.has_key("error"), True)
-#              
+         
+    def test100_942ShouldReturnValuesWithErrorWhenObservationXOutOfLowerBound(self):
+        self.setParm('op','adjust')
+        self.setParm('observation','0d15.2')        
+        result = nav.dispatch(self.inputDictionary)
+        self.assertTrue(result.has_key("error"), True)
+              
 #     def test100_943ShouldReturnValuesWithErrorWhenObservationYOutOfUpperBound(self):
 #         self.setParm('op','adjust')
 #         self.setParm('observation','45d12')        
