fe2b6ee8b3bb5f9a4c08118b9c8b9f89f13ccebb
Angus <Angus@DESKTOP-OB7QC8A>
2019-02-21 19:13:12 -0600

    Green Light |

--- nav/test/dispatchTest.py
+++ nav/test/dispatchTest.py
@@ -204,14 +204,14 @@ class DispatchTest(unittest.TestCase):
 #         result = self.microservice()
 #         resultDictionary = self.string2dict(result)
 #         self.assertTrue(resultDictionary.has_key("error"), True)
-#     
-#     def test100_961ShouldReturnValuesWithErrorWhenTemperatureOutOfUpperBound(self):
-#         self.setParm('op','adjust')
-#         self.setParm('observation','45d12')
-#         self.setParm('temperature','123')
-#         result = self.microservice()
-#         resultDictionary = self.string2dict(result)
-#         self.assertTrue(resultDictionary.has_key("error"), True)
+     
+    def test100_961ShouldReturnValuesWithErrorWhenTemperatureOutOfUpperBound(self):
+        self.setParm('op','adjust')
+        self.setParm('observation','45d12')
+        self.setParm('temperature','123')
+        result = self.microservice()
+        resultDictionary = self.string2dict(result)
+        self.assertTrue(resultDictionary.has_key("error"), True)
 #             
 #     def test100_962ShouldReturnValuesWithErrorWhenTemperatureOutOfLowerBound(self):
 #         self.setParm('op','adjust')
@@ -221,13 +221,13 @@ class DispatchTest(unittest.TestCase):
 #         resultDictionary = self.string2dict(result)
 #         self.assertTrue(resultDictionary.has_key("error"), True)
 #             
-    def test100_970ShouldReturnValuesWithErrorWhenPressureNotInteger(self):
-        self.setParm('op','adjust')
-        self.setParm('observation','45d12')
-        self.setParm('pressure','98')
-        result = self.microservice()
-        resultDictionary = self.string2dict(result)
-        self.assertTrue(resultDictionary.has_key("error"), True)
+#     def test100_970ShouldReturnValuesWithErrorWhenPressureNotInteger(self):
+#         self.setParm('op','adjust')
+#         self.setParm('observation','45d12')
+#         self.setParm('pressure','a')
+#         result = self.microservice()
+#         resultDictionary = self.string2dict(result)
+#         self.assertTrue(resultDictionary.has_key("error"), True)
 #            
 #     def test100_971ShouldReturnValuesWithErrorWhenPressureOutOfUpperBound(self):
 #         self.setParm('op','adjust')
