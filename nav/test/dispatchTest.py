d497504267c483b788aaf608879e76a304fbdbf8
Angus <Angus@131.204.232.75>
2019-03-08 12:31:14 -0600

    Green Light |

--- nav/test/dispatchTest.py
+++ nav/test/dispatchTest.py
@@ -283,7 +283,7 @@ class DispatchTest(unittest.TestCase):
 
 # acceptance test for predict
      
-    def test200_010ShouldReturnCorrectAltitude(self):
+    def test200_010ShouldReturnCorrectResult(self):
         self.setParm('op','predict')
         self.setParm('body','Aldebaran')
         self.setParm('date','2016-01-17')
@@ -295,4 +295,16 @@ class DispatchTest(unittest.TestCase):
         self.assertEquals(result['time'], '03:15:42')
         self.assertEquals(result['long'], '95d41.6')       
         self.assertEquals(result['lat'], '16d32.3') 
-      
+    
+    def test200_015ShouldReturnCorrectResult(self):
+        self.setParm('op','predict')
+        self.setParm('body','Betelgeuse')
+        self.setParm('date','2016-01-17')
+        self.setParm('time','03:15:42')
+        result = nav.dispatch(self.inputDictionary)
+        self.assertEquals(result['op'], 'predict')
+        self.assertEquals(result['body'], 'Aldebaran')
+        self.assertEquals(result['date'], '2016-01-17')
+        self.assertEquals(result['time'], '03:15:42')
+        self.assertEquals(result['long'], '75d53.6')       
+        self.assertEquals(result['lat'], '7d24.3')  
