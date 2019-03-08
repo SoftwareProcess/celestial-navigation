028774e0472ca2628a55183b3928c5084b8c4062
Angus <Angus@DESKTOP-OB7QC8A>
2019-03-07 21:19:07 -0600

    Green Light |

--- nav/test/predictTest.py
+++ nav/test/predictTest.py
@@ -112,7 +112,7 @@ class predictTest(unittest.TestCase):
     def test100_060ShouldCalculateCumulativeProgression(self):
         self.setParm('body','Aldebaran')
         self.setParm('date','2016-01-17')
-        expectedResult = '-214.75'
+        expectedResult = -214.75
         actualResult = nav.predict(self.inputDictionary)
         self.assertAlmostEquals(actualResult, expectedResult)
      
