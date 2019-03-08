55492c894dcae47c34eeb56598c78e3f6f9946c1
Angus <Angus@DESKTOP-OB7QC8A>
2019-03-07 18:50:14 -0600

    Green Light |

--- nav/predict.py
+++ nav/predict.py
@@ -8,7 +8,9 @@ def predict(values = None):
     workbook = xlrd.open_workbook('C:\Users\Angus\Desktop\stardata.xlsx')
     sheet = workbook.sheet_by_index(0)
     sheet.cell_value(0, 0)
+    result = 'not found'
+    for i in range(sheet.nrows):
+        if (sheet.cell_value(i, 0) == values['body']):
+            result = 'found'
     
-    
-    
-    return sheet.ncols
+    return result
