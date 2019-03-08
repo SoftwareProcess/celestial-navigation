7083a4be2f570c2a847fbf71630a69c6a8d9293c
Angus <Angus@DESKTOP-OB7QC8A>
2019-03-07 23:08:57 -0600

    Green Light |

--- nav/test/predictTest.py
+++ nav/test/predictTest.py
@@ -117,13 +117,13 @@ class predictTest(unittest.TestCase):
 #         actualResult = nav.predict(self.inputDictionary)
 #         self.assertAlmostEquals(actualResult, expectedResult, 5)
         
-    def test100_070ShouldCalculateLeapProgression(self):
-        self.setParm('body','Aldebaran')
-        self.setParm('date','2016-01-17')
-        self.setParm('time','03:15:42')
-        expectedResult = 176.925
-        actualResult = nav.predict(self.inputDictionary)
-        self.assertAlmostEquals(actualResult, expectedResult, 5)
+#     def test100_070ShouldCalculateLeapProgression(self):
+#         self.setParm('body','Aldebaran')
+#         self.setParm('date','2016-01-17')
+#         self.setParm('time','03:15:42')
+#         expectedResult = 176.925
+#         actualResult = nav.predict(self.inputDictionary)
+#         self.assertAlmostEquals(actualResult, expectedResult, 5)
      
 #     def test100_080ShouldCalculateSeconds(self):
 #         self.setParm('body','Aldebaran')
@@ -133,11 +133,11 @@ class predictTest(unittest.TestCase):
 #         actualResult = nav.predict(self.inputDictionary)
 #         self.assertAlmostEquals(actualResult, expectedResult, 3) 
            
-#     def test100_080ShouldCalculateTotalGHAAries(self):
-#         self.setParm('body','Aldebaran')
-#         self.setParm('date','2016-01-17')
-#         self.setParm('time','03:15:42')
-#         expectedResult = 6004.8
-#         actualResult = nav.predict(self.inputDictionary)
-#         self.assertAlmostEquals(actualResult, expectedResult, 3)  
+    def test100_080ShouldCalculateTotalGHAAries(self):
+        self.setParm('body','Aldebaran')
+        self.setParm('date','2016-01-17')
+        self.setParm('time','03:15:42')
+        expectedResult = 9894.5
+        actualResult = nav.predict(self.inputDictionary)
+        self.assertAlmostEquals(actualResult, expectedResult, 3)  
       
