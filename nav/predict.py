eee265915fb7b7f0f3b40a2d2a06eabb443e1785
Angus <Angus@DESKTOP-OB7QC8A>
2019-03-07 18:42:05 -0600

    Red Light |

--- nav/predict.py
+++ nav/predict.py
@@ -1,5 +1,10 @@
 from math import sqrt, tan, pi
+import xlrd
 
 def predict(values = None):
-
-    return values
+    
+    workbook = xlrd.open_workbook('C:\Users\Angus\Desktop\stardata.xlsx')
+    sheet = workbook.sheet_by_index(0)
+    sheet.cell_value(0, 0)
+    
+    return sheet.ncols
