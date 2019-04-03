9e94aae99291d2814de99d1cc848471ea60e22ef
Tianshi Che <tzc0029@auburn.edu>
2019-04-03 16:59:14 -0500

    Red Light |

--- nav/test/correctTest.py
+++ nav/test/correctTest.py
@@ -344,7 +344,15 @@ class correctTest(unittest.TestCase):
         actualResult = dispatch.dispatch(self.inputDictionary)
         self.assertEquals(actualResult['error'], 'lat is not valid')
         
-        
+    def test900_051ReturnErrorIfMinutesIsNotNumerical(self):
+        self.setParm('op','correct')
+        self.setParm('lat','89dabc')
+        self.setParm('long','0154d05.4')
+        self.setParm('altitude','037d015.6')
+        self.setParm('assumedLat','033d059.7')
+        self.setParm('assumedLong','074d035.3')
+        actualResult = dispatch.dispatch(self.inputDictionary)
+        self.assertEquals(actualResult['error'], 'lat is not valid')    
         
         
         
