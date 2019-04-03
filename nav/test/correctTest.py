68cf388eb9dd5f8897924b1f4c86c86c3dba8485
Tianshi Che <tzc0029@auburn.edu>
2019-04-02 22:05:03 -0500

    Green Light |

--- nav/test/correctTest.py
+++ nav/test/correctTest.py
@@ -205,5 +205,8 @@ class correctTest(unittest.TestCase):
         actualResult = dispatch.dispatch(self.inputDictionary)
         self.assertEquals(actualResult['error'], 'mandatory information is missing')
 
-
+    def test900_010LatIsNotValid(self):       
+        self.setParm('op','correct')
+        actualResult = dispatch.dispatch(self.inputDictionary)
+        self.assertEquals(actualResult['error'], 'lat is not valid')
 
