01a2987356f3cf705de0ee0f5a9166eadf9334a0
Tianshi Che <tzc0029@auburn.edu>
2019-04-30 03:53:20 -0500

    Red Light |

--- nav/locate.py
+++ nav/locate.py
@@ -4,4 +4,16 @@ def locate(values = None):
     
     
     
-    return values;
+    correctionString = values['corrections']
+    correctionString = correctionString[1:-1]
+    tempList = []
+    for i in range(len(correctionString)):
+        temp = ''
+        if (correctionString[i] == '['):
+            while (correctionString[i+1] != ']'):
+                temp += correctionString[i+1]
+                i += 1
+            tempList.append(temp)
+    numOfCorrections = len(tempList)
+    
+    return numOfCorrections;
