b9e5a51c158aa2c2a073572d93ec83973d964b4f
Tianshi Che <tzc0029@auburn.edu>
2019-05-01 01:20:40 -0500

    Red Light |

---
+++ nav/locate.py
@@ -0,0 +1,88 @@
+from math import cos 
+from math import radians 
+from math import sin 
+from math import sqrt 
+from math import pow
+
+
+
+def locate(values = None):
+    
+    
+    def convertStrToDegrees(arg):
+        x, y = arg.split('d')
+        if (int(x) >= 0):
+            degrees = int(x) + float(y)/60
+        else:
+            degrees = int(x) - float(y)/60
+        return degrees
+    
+    def convertStrToMinutes(arg):
+        x, y = arg.split('d')
+        if (int(x) >= 0):
+            minutes = int(x) * 60 + float(y)
+        else:
+            minutes = int(x) * 60 - float(y)
+        return minutes
+
+    def convertMinutesToStr(arg):
+        if (arg >= 0):
+            degree = int(arg / 60)
+            minutes = round(arg % 60, 1)
+        else:
+            degree = int(arg / 60)
+            minutes = -(round(arg % -60, 1))   
+        string = str(degree) + 'd' + str(minutes)           
+        return string  
+    
+    def myRound(arg):
+        temp = abs(arg)
+        decimal = temp - int(temp)
+        if (decimal < 0.5):
+            temp = int(temp)
+        else:
+            temp = int(temp) + 1
+        if (arg < 0):
+            temp = -temp
+        return temp  
+ 
+    correctionString = values['corrections']
+    correctionString = correctionString[1:-1]
+    tempList = []
+    for i in range(len(correctionString)):
+        temp = ''
+        if (correctionString[i] == '['):
+            while (correctionString[i+1] != ']'):
+                temp += correctionString[i+1]
+                i += 1
+            tempList.append(temp)
+    numOfCorrections = len(tempList)
+
+    tempSum = 0
+    for i in range(numOfCorrections):
+        corDis, corAzm = tempList[i].split(',') 
+        tempSum = tempSum + float(corDis) * cos(radians(convertStrToDegrees(corAzm))) 
+    nsCorrection = tempSum / numOfCorrections
+    
+    tempSum = 0
+    for i in range(numOfCorrections):
+        corDis, corAzm = tempList[i].split(',') 
+        tempSum = tempSum + float(corDis) * sin(radians(convertStrToDegrees(corAzm))) 
+    ewCorrection = tempSum / numOfCorrections
+    
+    presentLat = convertMinutesToStr((convertStrToMinutes(values['assumedLat']) + nsCorrection))
+    presentLong = convertMinutesToStr((convertStrToMinutes(values['assumedLong']) + ewCorrection))
+    
+    tempSum = 0
+    for i in range(numOfCorrections):
+        corDis, corAzm = tempList[i].split(',') 
+        tempSum = tempSum + sqrt(pow((float(corDis) * cos(radians(convertStrToDegrees(corAzm))) - nsCorrection), 2) + 
+                                 pow((float(corDis) * sin(radians(convertStrToDegrees(corAzm))) - ewCorrection), 2))
+    percision = float(1)/numOfCorrections * tempSum                         
+    percision = myRound(percision)
+                           
+   
+    
+    values['percision'] = str(percision)
+    
+    return values;
---
+++ nav/test/locateTest.py
@@ -0,0 +1,164 @@
+import unittest
+import httplib
+from urllib import urlencode
+import json
+import nav.locate as nav
+import nav.dispatch as dispatch
+
+class locateTest(unittest.TestCase):
+    def setUp(self):
+        self.inputDictionary = {}
+        self.errorKey = "error"
+        self.solutionKey = "probability"
+        self.BX_PATH = '/nav?'
+        self.BX_PORT = 5000
+        self.BX_URL = 'localhost'
+#         self.BX_PORT = 5000
+#         self.BX_URL = 'www.ibmcloud.com'
+
+    def tearDown(self):
+        self.inputDictionary = {}
+
+    def setParm(self, key, value):
+        self.inputDictionary[key] = value
+        
+    def microservice(self):
+        try:
+            theParm = urlencode(self.inputDictionary)
+            theConnection = httplib.HTTPConnection(self.BX_URL, self.BX_PORT)
+            theConnection.request("GET", self.BX_PATH + theParm)
+            theStringResponse = theConnection.getresponse().read()
+            return theStringResponse
+        except Exception as e:
+            return "error encountered during transaction"
+        
+    def string2dict(self, httpResponse):
+        '''Convert JSON string to dictionary'''
+        result = {}
+        try:
+            jsonString = httpResponse.replace("'", "\"")
+            unicodeDictionary = json.loads(jsonString)
+            for element in unicodeDictionary:
+                if(isinstance(unicodeDictionary[element],unicode)):
+                    result[str(element)] = str(unicodeDictionary[element])
+                else:
+                    result[str(element)] = unicodeDictionary[element]
+        except Exception as e:
+            result['diagnostic'] = str(e)
+        return result
+
+
+#     def test100_010ShouldVerifyCallToLocate(self):
+#         expectedResult = 1.0
+#         actualResult = nav.locate(expectedResult)
+#         self.assertEquals(expectedResult, actualResult)
+#         
+#         
+#     def test100_020ShouldReturnDictionary(self):
+#         self.setParm('op','locate')
+#         self.setParm('assumedLat','-53d38.4')
+#         self.setParm('assumedLong','350d35.3') 
+#         self.setParm('corrections','[[100,1d0.1]]')
+#         expectedResult = {'assumedLat': '-53d38.4', 
+#                           'assumedLong': '350d35.3', 
+#                           'corrections': '[[100,1d0.1]]',   
+#                           'op': 'locate'}     
+#         actualResult = nav.locate(self.inputDictionary)
+#         self.assertEquals(expectedResult, actualResult)
+
+#     def test100_021ShouldReadHowManyElementsInCorrections(self):
+#         self.setParm('op','locate')
+#         self.setParm('assumedLat','-53d38.4')
+#         self.setParm('assumedLong','350d35.3') 
+#         self.setParm('corrections','[[100,1d0.1]]')
+#         expectedResult = 1     
+#         actualResult = nav.locate(self.inputDictionary)
+#         self.assertEquals(expectedResult, actualResult)
+
+#     def test100_021ShouldReturnExactElementAsStringInCorrections(self):
+#         self.setParm('op','locate')
+#         self.setParm('assumedLat','-53d38.4')
+#         self.setParm('assumedLong','350d35.3') 
+#         self.setParm('corrections','[[100,1d0.1]]')
+#         expectedResult = '100,1d0.1'    
+#         actualResult = nav.locate(self.inputDictionary)
+#         self.assertEquals(expectedResult, actualResult)
+
+
+#     def test100_030ShouldReturnNSCorrection(self):
+#         self.setParm('op','locate')
+#         self.setParm('assumedLat','-53d38.4')
+#         self.setParm('assumedLong','350d35.3') 
+#         self.setParm('corrections','[[50,45d0.0]]')
+#         expectedResult = 35.36    
+#         actualResult = nav.locate(self.inputDictionary)
+#         self.assertAlmostEquals(expectedResult, actualResult, 2)
+
+    
+#     def test100_031ShouldReturnNSCorrection(self):
+#         self.setParm('op','locate')
+#         self.setParm('assumedLat','-53d38.4')
+#         self.setParm('assumedLong','350d35.3') 
+#         self.setParm('corrections','[[50,45d0.0], [75,60d42.0],[100,300d11.2],[42,42d12.3],[70,60d45.0],[10,280d0.0]]')
+#         expectedResult = 31.57    
+#         actualResult = nav.locate(self.inputDictionary)
+#         self.assertAlmostEquals(expectedResult, actualResult, 2)
+
+#     def test100_040ShouldReturnEWCorrection(self):
+#         self.setParm('op','locate')
+#         self.setParm('assumedLat','-53d38.4')
+#         self.setParm('assumedLong','350d35.3') 
+#         self.setParm('corrections','[[50,45d0.0], [75,60d42.0],[100,300d11.2],[42,42d12.3],[70,60d45.0],[10,280d0.0]]')
+#         expectedResult = 15.63    
+#         actualResult = nav.locate(self.inputDictionary)
+#         self.assertAlmostEquals(expectedResult, actualResult, 2)
+
+#     def test100_040ShouldReturnPresentLat(self):
+#         self.setParm('op','locate')
+#         self.setParm('assumedLat','32d36.5')
+#         self.setParm('assumedLong','274d31.1') 
+#         self.setParm('corrections','[[50,45d0.0], [75,60d42.0],[100,300d11.2],[42,42d12.3],[70,60d45.0],[10,280d0.0]]')
+#         expectedResult = '33d8.1'    
+#         actualResult = nav.locate(self.inputDictionary)
+#         self.assertEquals(expectedResult, actualResult)
+        
+#     def test100_041ShouldReturnPresentLong(self):
+#         self.setParm('op','locate')
+#         self.setParm('assumedLat','32d36.5')
+#         self.setParm('assumedLong','274d31.1') 
+#         self.setParm('corrections','[[50,45d0.0], [75,60d42.0],[100,300d11.2],[42,42d12.3],[70,60d45.0],[10,280d0.0]]')
+#         expectedResult = '274d46.7'    
+#         actualResult = nav.locate(self.inputDictionary)
+#         self.assertEquals(expectedResult, actualResult)    
+        
+    def test100_041ShouldReturnPrecision(self):
+        self.setParm('op','locate')
+        self.setParm('assumedLat','32d36.5')
+        self.setParm('assumedLong','274d31.1') 
+        self.setParm('corrections','[[50,45d0.0], [75,60d42.0],[100,300d11.2],[42,42d12.3],[70,60d45.0],[10,280d0.0]]')         
+        actualResult = nav.locate(self.inputDictionary)
+        self.assertEquals(actualResult['percision'], '45')         
+        
+#     def test100_041ShouldReturnPrecisionStepOne(self):
+#         self.setParm('op','locate')
+#         self.setParm('assumedLat','32d36.5')
+#         self.setParm('assumedLong','274d31.1') 
+#         self.setParm('corrections','[[50,45d0.0], [75,60d42.0],[100,300d11.2],[42,42d12.3],[70,60d45.0],[10,280d0.0]]') 
+#         expectedResult = 403.56        
+#         actualResult = nav.locate(self.inputDictionary)
+#         self.assertAlmostEquals(expectedResult, actualResult, 2)         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
