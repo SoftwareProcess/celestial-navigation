94698c0fbd2ccc97c9d8b6aa4653d698c648e832
Tianshi Che <tzc0029@auburn.edu>
2019-05-01 02:37:18 -0500

    Green Light |

--- nav/locate.py
+++ nav/locate.py
@@ -69,7 +69,7 @@ def locate(values = None):
     def convex_hull(points):
         points = sorted(set(points))
         if len(points) < 3:
-            return NULL
+            return None
         def cross(o, a, b):
             return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
 
