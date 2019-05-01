f83f8b76ea46cff05c60674509226f8905f1a8d2
Tianshi Che <tzc0029@auburn.edu>
2019-05-01 06:03:51 -0500

    Green Light |

--- nav/test/locateTest.py
+++ nav/test/locateTest.py
@@ -225,3 +225,12 @@ class locateTest(unittest.TestCase):
         actualResult = nav.locate(self.inputDictionary)
         self.assertEquals(actualResult['error'], 'correction is not valid')
     
+    def test900_050ShouldReturnErrorWhenElementInCorrectionsIsNotvalid(self):
+        self.setParm('op','locate')
+        self.setParm('assumedLat','50d0.0')
+        self.setParm('assumedLong','350d35.3') 
+        self.setParm('corrections','[[100,1d0.0], (100, 50d0.0)]')        
+        actualResult = nav.locate(self.inputDictionary)
+        self.assertEquals(actualResult['error'], 'correction is not valid')
+    
+    
